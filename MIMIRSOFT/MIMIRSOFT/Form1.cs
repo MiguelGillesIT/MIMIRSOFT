4ee03d3fe57c16d94b34660dd43ace8f8d9957b2
MiguelGilles <miguelgilles9@gmail.com>
2022-06-17 17:02:32 +0000

    Dynamically add Network Interface to ToolStrip

--- MIMIRSOFT/MIMIRSOFT/Form1.cs
+++ MIMIRSOFT/MIMIRSOFT/Form1.cs
@@ -1,3 +1,6 @@
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
 namespace MIMIRSOFT
 {
     public partial class Form1 : Form
@@ -5,8 +8,32 @@ namespace MIMIRSOFT
         public Form1()
         {
             InitializeComponent();
+            InitializeNetWorkAdaptater();
+
         }
 
+        public void InitializeNetWorkAdaptater()
+        {
+            foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                foreach (UnicastIPAddressInformation unicastIPAddressInformation in adapter.GetIPProperties().UnicastAddresses)
+                {
+                    if (unicastIPAddressInformation.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        if(adapter.OperationalStatus == OperationalStatus.Up)
+                        {
+                            if(adapter.Name != "Loopback Pseudo-Interface 1"){
+
+                                carteRéseauToolStripMenuItem.DropDown.Items.Add(adapter.Name);
+
+                            }
+                            
+                        }
+
+                    }
+                }
+            }
+        }
     
     }
 }
